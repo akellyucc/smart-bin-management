f5941706db0debe82991a9c2c95bda6cf8649a42
akellyucc <akelly13@stu.ucc.edu.jm>
2024-11-02 02:06:26 -0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-
 const express = require('express');
 
 const app = express();
 
+app.use("/", (req, res) => {
+    res.send("Server is running.");
+});
 
-
-app.use("/",(req,res)=>{
-     res.send("Server is running.");
-   });
-
-app.listen(3001, console.log("Server started on PORT 3001"));
+// Start the server and log a message when it's ready
+app.listen(3001, () => {
+    console.log("Server started on PORT 3001");
+});
